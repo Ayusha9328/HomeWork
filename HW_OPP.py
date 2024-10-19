32bd1ec0748a52f0a9c83525890e1bfe047f8565
Oshorov <metslujba@gmail.com>
2024-10-19 21:53:42 +0800

    changes

--- HW_OPP.py
+++ HW_OPP.py
@@ -1,11 +1,12 @@
 class Student:
-    def __init__(self, name, surname, gender):
+    def __init__(self, name, surname, gender, average):
         self.name = name
         self.surname = surname
         self.gender = gender
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
+        self.average = average
 
     def rate_lecturer(self, lecturer, course, grade):
         if isinstance(lecturer, Lecturer) and course in self.courses_in_progress:
@@ -26,6 +27,20 @@ class Student:
                 f"Средняя оценка за домашние задания: {self.average_grade()}, "
                 f"Курсы в процессе изучения: {self.courses_in_progress}, "
                 f"Завершенные курсы: {self.finished_courses}")
+    
+    def __eq__(self, average_st):
+        return (self.average == average_st.average)
+    
+    def calculate_aver(student, course_name):
+        total = 0
+        count = 0
+        if course_name in student.grades:
+            total += student.grades[course_name]
+            count += 1 
+        
+        if count == 0:
+            return 0
+        return total / count    
 
 
 class Mentor:
@@ -38,9 +53,10 @@ class Mentor:
 
 
 class Lecturer(Mentor):
-    def __init__(self, name, surname):
+    def __init__(self, name, surname, average):
         super().__init__(name, surname)
-        self.grades = {} 
+        self.grades = {}
+        self.average = average
 
     def average_grade(self):
         total = 0
@@ -50,9 +66,19 @@ class Lecturer(Mentor):
             count += len(grades)
         return total / count if count > 0 else 0 
 
-    def __str__(self):
-        return (f"Имя: {self.name}, Фамилия: {self.surname}, "
-                f"Средняя оценка за лекции: {self.average_grade()}")
+    def __eq__(self, average_lt):
+        return (self.average == average_lt.average)
+    
+    def calculate_aver(lecturer, course_name):
+        total = 0
+        count = 0
+        if course_name in lecturer.grades:
+            total += lecturer.grades[course_name]
+            count += 1 
+        
+        if count == 0:
+            return 0
+        return total / count 
 
 
 class Reviewer(Mentor):
@@ -70,7 +96,7 @@ class Reviewer(Mentor):
             return 'Ошибка'       
       
 # Пример использования
-best_student = Student('Ruoy', 'Eman', 'your_gender')
+best_student = Student('Ruoy', 'Eman', 'your_gender' , 'average')
 best_student.courses_in_progress += ['Python', 'Git']
 best_student.finished_courses = ['Введение в программирование']
 print(best_student)
@@ -82,10 +108,16 @@ cool_mentor.rate_hw(best_student, 'Python', 10)
 cool_mentor.rate_hw(best_student, 'Python', 10)
 cool_mentor.rate_hw(best_student, 'Python', 10)
 
-lecturer1 = Lecturer("Иванов", "И.И.")  # Добавляем surname
+lecturer1 = Lecturer("Иванов", "И.И.", 'average')  # Добавляем surname
 best_student.rate_lecturer(lecturer1, 'Python', 9)
 best_student.rate_lecturer(lecturer1, 'Python', 5)
 best_student.rate_lecturer(lecturer1, 'Python', 10)
+
+# Сравнение
+aver_st = best_student.average_grade()
+aver_lt = lecturer1.average_grade()
+
+comaparision = aver_st == aver_lt
 
 # Проверка
 print('Оценки студента:', best_student.grades)  
@@ -93,3 +125,4 @@ print('Оценки лектора:', lecturer1.grades)
 
 print(f'Средняя оценка студента: {best_student.average_grade()}')  # Вычисление средней оценки студента
 print(f'Средняя оценка лектора: {lecturer1.average_grade()}')  # Вычисление средней оценки лектора
+print(f'Сравнение средних оценок лекторов и студентов: {comaparision}')
